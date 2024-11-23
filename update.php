b0307f8c4278fd45549f53496a03609622a4405a
sailijar <sailijar88@gmail.com>
2024-11-23 12:05:01 +0700

    membuat file update

---
+++ update.php
@@ -0,0 +1,23 @@
+<?php
+#1. koneksikan file ini
+include("koneksi.php");
+
+#2. mengambil value dari form
+$id = $_POST['id_buku'];
+$jb = $_POST['judul_buku'];
+$pgr = $_POST['pengarang'];
+$ktr = $_POST['kategori'];
+$thn = $_POST['tahun_terbit'];
+
+#3. menulis query
+$sunting = "UPDATE buku SET pengarang='$pgr', buku='$jb' WHERE id_buku='$id'";
+
+#4. jalankan query
+$proses = mysqli_query($koneksi, $sunting);
+
+#5. mengalihkan halaman
+// header("location:index.php");
+?>
+<script>
+    document.location="index.php";
+</script>
