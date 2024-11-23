7b42a5c07853579f5b863dd4bfb6e7d9bf40a616
sailijar <sailijar88@gmail.com>
2024-11-23 12:21:30 +0700

    ubah di file update

--- update.php
+++ update.php
@@ -3,14 +3,14 @@
 include("koneksi.php");
 
 #2. mengambil value dari form
-$id = $_POST['id_buku'];
+$id = $_POST['id'];
 $jb = $_POST['judul_buku'];
 $pgr = $_POST['pengarang'];
 $ktr = $_POST['kategori'];
 $thn = $_POST['tahun_terbit'];
 
 #3. menulis query
-$sunting = "UPDATE buku SET pengarang='$pgr', buku='$jb' WHERE id_buku='$id'";
+$sunting = "UPDATE buku SET pengarang='$pgr', buku='$jb' WHERE id='$id'";
 
 #4. jalankan query
 $proses = mysqli_query($koneksi, $sunting);
